ae629e6107b680a8d4ed52878ec3506e3e9bca55
keepforever <briancilenti@gmail.com>
2019-02-07 16:43:33 -0800

    added examples

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default class App extends Component {
   nameRef = React.createRef();
 
   state = {
+    selectedOption: "option1",
+    hasBike: false,
+    hasSpine: true,
     details: { eventName: "Birthday Party" },
     people: [
       { uuid: uuid(), name: "Leigh", attending: true },
@@ -15,6 +18,12 @@ export default class App extends Component {
       { uuid: uuid(), name: "Dorothy", attending: false },
       { uuid: uuid(), name: "Grandma", attending: true }
     ]
+  };
+
+  handleOptionChange = e => {
+    this.setState({
+      selectedOption: e.target.value
+    });
   };
 
   setEventName = value => {
@@ -80,8 +89,28 @@ export default class App extends Component {
   }));
 };
 
+  toggleHasBike = name => event => {
+    console.log('\n', 'event.target.checked', '\n', '\n', event.target.checked )
+    this.setState({
+      ...this.state,
+      [name]: event.target.checked,
+    })
+  };
+
+  toggleHasSpine = event => {
+    console.log('\n', 'event.target.name', '\n', '\n', event.target.name )
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.checked,
+    })
+  };
+
   render() {
-    const { details, people } = this.state;
+    const { details, people, hasBike, hasSpine } = this.state;
+
+    console.log('\n', 'hasBike', '\n', hasBike, '\n', 'hasSpine', '\n', hasSpine, '\n',  )
+
+    console.log('\n', 'this.state', '\n', '\n', this.state )
 
     return (
       <div className="App">
@@ -128,16 +157,79 @@ export default class App extends Component {
           <input ref={this.nameRef} placeholder="Attendee's name" required />
           <button>Add</button>
         </form>
-        <br/>
-        <hr/>
-        <hr/>
-        <hr/>
-        <br/>
+        <br/> <hr/> <hr/> <hr/>  <br/>
         <h1>
           CRUD
         </h1>
         <Crud />
+        <br/> <hr/> <hr/> <hr/>  <br/>
+        <h1>
+          Checkbox
+        </h1>
+        <input
+          onChange={this.toggleHasSpine}
+          type="checkbox"
+          name="hasSpine"
+          checked={hasSpine}
+        />
+        <span style={{marginLeft: 20}}>
+          I have a spine
+        </span>
 
+        <br/>
+
+        <input
+          onChange={this.toggleHasBike("hasBike")}
+          type="checkbox"
+          checked={hasBike}
+        />
+        <span style={{marginLeft: 20}}>
+          I have a bike
+        </span>
+        <br />
+
+        <h1>Radio</h1>
+        <div className="form-check">
+          <label>
+            <input
+              type="radio"
+              name="react-tips"
+              value="option1"
+              checked={this.state.selectedOption === "option1"}
+              className="form-check-input"
+              onChange={this.handleOptionChange}
+            />
+            Option 1
+          </label>
+        </div>
+
+        <div className="form-check">
+          <label>
+            <input
+              type="radio"
+              name="react-tips"
+              value="option2"
+              checked={this.state.selectedOption === "option2"}
+              className="form-check-input"
+              onChange={this.handleOptionChange}
+            />
+            Option 2
+          </label>
+        </div>
+
+        <div className="form-check">
+          <label>
+            <input
+              type="radio"
+              name="react-tips"
+              value="option3"
+              checked={this.state.selectedOption === "option3"}
+              className="form-check-input"
+              onChange={this.handleOptionChange}
+            />
+            Option 3
+          </label>
+        </div>
       </div>
     );
   }
